f79b1acc2bfc0af995785db008ee5c6f41f14aa6
codernesia <webmastermonsternesia@gmail.com>
2018-08-21 07:33:16 +0700

    Number Spelling

---
+++ Number Spelling/NumberSpelling.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'gui.ui'
+#
+# Created by: PyQt5 UI code generator 5.5.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from num2words import num2words
+from math import*
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(555, 267)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(80, 80, 67, 17))
+        self.label.setObjectName("label")
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(QtCore.QRect(80, 130, 91, 17))
+        self.label_2.setObjectName("label_2")
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit.setGeometry(QtCore.QRect(160, 80, 301, 27))
+        self.lineEdit.setObjectName("lineEdit")
+        self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
+        self.lineEdit_2.setGeometry(QtCore.QRect(160, 130, 301, 27))
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(250, 170, 99, 27))
+        self.pushButton.setObjectName("pushButton")
+        self.pushButton.clicked.connect(self.hello)
+        
+        
+        self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_2.setGeometry(QtCore.QRect(360, 170, 99, 27))
+        self.pushButton_2.setObjectName("pushButton_2")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        self.pushButton_2.clicked.connect(self.lineEdit_2.clear)
+        self.pushButton_2.clicked.connect(self.lineEdit.clear)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Number to Words"))
+        self.label.setText(_translate("MainWindow", "Bilangan : "))
+        self.label_2.setText(_translate("MainWindow", "Terbilang : "))
+        self.pushButton.setText(_translate("MainWindow", "Spell Number"))
+        self.pushButton_2.setText(_translate("MainWindow", "Clear"))
+
+    def hello(self):
+        Bilangan = self.lineEdit.text()
+        Terbilang = int(float(Bilangan))
+        self.lineEdit_2.setText(str(num2words(Terbilang)))
+       
+
+
